b198fef76546b4f5dfac404bffce95d6f796a4f5
crassolini <crassolini@yahoo.com.br>
2022-07-29 09:43:59 -0300

    Primeiro endpoint

---
+++ src/main/java/com/teste/parking/controller/ParkingController.java
@@ -0,0 +1,32 @@
+package com.teste.parking.controller;
+
+import com.teste.parking.controller.dto.ParkingDTO;
+import com.teste.parking.controller.mapper.ParkingMapper;
+import com.teste.parking.model.Parking;
+import com.teste.parking.service.ParkingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+    @GetMapping
+    public List<ParkingDTO> findAll() {
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
+    }
+}
---
+++ src/main/java/com/teste/parking/controller/mapper/ParkingMapper.java
@@ -0,0 +1,22 @@
+package com.teste.parking.controller.mapper;
+
+import com.teste.parking.controller.dto.ParkingDTO;
+import com.teste.parking.model.Parking;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO parkingDTO(Parking parking) {
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
+        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+    }
+}
